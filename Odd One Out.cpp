7b792e29cc9cc2b7d50a3263cf13df093c8a3673
xFGhoul <dauntejules9@gmail.com>
2024-04-01 12:18:00 -0700

    completed?

--- Odd One Out.cpp
+++ Odd One Out.cpp
@@ -2,67 +2,208 @@
 //
 
 #include <iostream>
-#include <vector>
-#include <map>
-#include <random>
-
-#include "Team.hpp"
+#include <fstream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+#include <windows.h>
 
 using namespace std;
 
 
-auto choose_random_guess(vector<map<string, bool>> guesses) {
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> distr(0, guesses.size() - 1);
-
-    int randomIndex = distr(gen);
-
-    auto random_guess = guesses[randomIndex];
-
-    return random_guess;
-}
- 
 int main()
 {
-    Team TeamA;
-    Team TeamB;
+    // Initialize Variables
 
-    vector<map<string, bool>> guesses = {
-        {{"something", false}, {"okay", true}, {"test", false}}
-    };
+    // Team Points
+    int team_a_points = 0;
+    int team_b_points = 0;
+    int points_needed_to_win = 20;
 
-    cout << "Welcome To Odd One Out" << endl;
+    // Variables to be used for Double Incorrect Logic
+    int team_a_wrong_answers = 0;
+    int team_b_wrong_answers = 0;
 
-    while (TeamA.points < 10 && TeamB.points < 10) {
+    // Choosing Which Team's Turn
+    bool team_a_turn = true;
+    bool team_b_turn = false;
 
+    // Varialbe Used For Reading Lines In File
+    int size = 0;
 
-        cout << "Team A Points: " << TeamA.points << endl;
-        cout << "Team B Points: " << TeamB.points << endl;
+    // Arrays That Stores Cards and Answers
+    string deck[5];
+    string answers[5];
+    string categories[5];
 
-        auto guess = choose_random_guess(guesses);
+    // User Answer (Reusable)
+    string answer;
 
-        cout << "Listing The Guesses" << endl;
-
-        for (auto kv : guess) {
-            cout << kv.first << endl;
-        }
-
-        string current_try;
-
-        cout << "Which one is the odd one out?: " << endl;
-        cin >> current_try;
-
-        for (auto kv : guess) {
-            if (kv.first == current_try && kv.second == true) {
-                cout << "You have guessed correctly! +2 points" << endl;
-                TeamA.increment(2);
-            }
-        }
+    // Card File Handler
+    ifstream Fcards("cards.txt");
+    if (!Fcards) {
+        cout << "Error Opening Cards File!" << endl;
+        return 0;
     }
 
-    // wait jn lemme share my whole screen
+    // Answers File Handler
+    ifstream Fanswers("answers.txt");
+    if (!Fanswers) {
+        cout << "Error Opening Answers File!" << endl;
+        return 0;
+    }
 
+    // Category File Handler
+    ifstream Fcategories("category.txt");
+    if (!Fcategories) {
+        cout << "Error Opening Categories File!" << endl;
+        return 0;
+    }
 
+    // Reading Files Into Arrays
+    string line;
+    while (getline(Fcards, line, '\n')) {
+        deck[size] = line;
+        size += 1;
+    }
+
+    size = 0; // reset size
+
+    while (getline(Fanswers, line, '\n')) {
+        answers[size] = line;
+        size += 1;
+    }
+
+    size = 0; // reset size
+
+    while (getline(Fcategories, line, '\n')) {
+        categories[size] = line;
+        size += 1;
+    }
+    
+    cout << "Welcome To Odd One Out!\n" << endl;
+
+    while (team_a_points < points_needed_to_win && team_b_points < points_needed_to_win) {
+
+        if (team_a_turn) {
+            // List Points
+            cout << "Team A Points: " << team_a_points << endl;
+            cout << "Team B Points: " << team_b_points << endl;
+
+            int random_card = rand() % 3;
+
+            cout << "\nDisplaying Options: " << endl;
+            cout << deck[random_card] << endl;
+
+            cout << "\nTeam A's Choice To Choose The Odd One Out" << endl;
+            cout << "Enter The Odd One Out: " << endl;
+            cin >> answer;
+
+            if (answer == answers[random_card]) {
+                cout << "\nCorrect!, Team A Guessed Correctly!" << endl;
+                team_a_points = team_a_points + 2;
+
+                if (team_a_points >= points_needed_to_win) {
+                    cout << "\nTeam A Won The Game!!!" << endl;
+                    break;
+                }
+            }
+            else {
+                team_a_wrong_answers = team_a_wrong_answers + 1;
+
+                cout << "\nTeam A Entered The Incorrect Odd One Out" << endl;
+                cout << "Team B You Now Have A Choice To Enter The Odd One Out" << endl;
+
+                cout << "\nEnter The Odd One Out: " << endl;
+                cin >> answer;
+
+                if (answer == answers[random_card]) {
+                    cout << "Team B Got It Correct" << endl;
+                    team_b_points = team_b_points + 1;
+
+                    if (team_b_points >= points_needed_to_win) {
+                        cout << "\nTeam B Won The Game!!!" << endl;
+                        break;
+                    }
+                }
+                else {
+                    cout << "Both Teams Got The Odd One Out Incorrect, No Points Awarded!" << endl;
+                }
+
+                if (team_a_wrong_answers >= 2) {
+                    cout << "Deducting 1 Point From Team A For Double Incorrect" << endl;
+                    team_a_points = team_a_points - 1;
+                    team_a_wrong_answers = 0; // reset points
+                }
+            }
+
+            cout << "The Category Was: " << categories[random_card] << endl;
+            cout << "The Answer Was: " << answers[random_card] << endl;
+
+            team_a_turn = false;
+            team_b_turn = true;
+
+            Sleep(3000);
+            system("cls");
+        }
+
+        if (team_b_turn) {
+            // List Points
+            cout << "Team A Points: " << team_a_points << endl;
+            cout << "Team B Points: " << team_b_points << endl;
+
+            int random_card = rand() % 3;
+
+            cout << "\nDisplaying Options: " << endl;
+            cout << deck[random_card] << endl;
+
+            cout << "\nTeam B's Choice To Choose The Odd One Out" << endl;
+            cout << "Enter The Odd One Out: " << endl;
+            cin >> answer;
+
+            if (answer == answers[random_card]) {
+                cout << "\nCorrect!, Team B Guessed Correctly!" << endl;
+                team_b_points = team_b_points + 2;
+            }
+            else {
+                team_b_wrong_answers = team_b_wrong_answers + 1;
+
+                cout << "\nTeam B Entered The Incorrect Odd One Out" << endl;
+                cout << "Team A You Now Have A Choice To Enter The Odd One Out" << endl;
+
+                cout << "\nEnter The Odd One Out: " << endl;
+                cin >> answer;
+
+                if (answer == answers[random_card]) {
+                    cout << "Team A Got It Correct" << endl;
+                    team_a_points = team_a_points + 1;
+                }
+                else {
+                    cout << "Both Teams Got The Odd One Out Incorrect, No Points Awarded!" << endl;
+                }
+
+                if (team_b_wrong_answers >= 2) {
+                    cout << "Deducting 1 Point From Team B For Double Incorrect" << endl;
+                    team_b_points = team_b_points - 1;
+                    team_b_wrong_answers = 0; // reset points
+                }
+            }
+
+            cout << "The Category Was: " << categories[random_card] << endl;
+            cout << "The Answer Was: " << answers[random_card] << endl;
+
+            team_a_turn = true;
+            team_b_turn = false;
+
+            Sleep(3000);
+            system("cls");
+        }
+
+    }
+
+    cout << "\nTeam A Points: " << team_a_points << endl;
+    cout << "Team B Points: " << team_b_points << endl;
+    
+    return 0;
 }
 
